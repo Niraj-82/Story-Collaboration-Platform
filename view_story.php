aa2e156c910fc28517979b3026188db805165f31
Shreyas <shreyas141105@gmail.com>
2025-04-15 09:56:27 +0530

    Initial commit

---
+++ view_story.php
@@ -0,0 +1,56 @@
+<?php
+include 'db.php';
+if(!isset($_GET['id'])) {
+  header('Location: stories.php');
+  exit;
+}
+
+$id = (int)$_GET['id'];
+$result = mysqli_query($conn, "SELECT s.*, u.username FROM stories s JOIN users u ON s.user_id = u.id WHERE s.id = $id");
+
+if(mysqli_num_rows($result) === 0) {
+  header('Location: stories.php');
+  exit;
+}
+
+$story = mysqli_fetch_assoc($result);
+?>
+<?php include 'header.php'; ?>
+
+  <div class="story-hero fade-in">
+    <div class="story-hero-bg" style="background-image: url('uploads/hero/<?php echo $id; ?>.jpg')"></div>
+    <div class="story-hero-content">
+      <h1><?php echo htmlspecialchars($story['title']); ?></h1>
+      <div class="story-meta-info">
+        <span><i class="fas fa-user"></i> <?php echo htmlspecialchars($story['username']); ?></span>
+        <span><i class="fas fa-clock"></i> Published</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="story-content-container fade-in">
+    <div class="story-content">
+      <?php 
+        $paragraphs = explode("\n", $story['content']);
+        foreach ($paragraphs as $para) {
+          echo "<p>" . nl2br(htmlspecialchars($para)) . "</p>";
+        }
+      ?>
+    </div>
+
+    <div class="story-actions">
+      <div class="story-share">
+        <a href="#"><img src="facebook.png" alt="Share on Facebook"></a>
+        <a href="#"><img src="twitter.png" alt="Share on Twitter"></a>
+        <a href="#"><img src="images/social/link.png" alt="Copy link"></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="night-mode-toggle" onclick="document.body.classList.toggle('night-mode')">
+    <i class="fas fa-moon"></i>
+  </div>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
